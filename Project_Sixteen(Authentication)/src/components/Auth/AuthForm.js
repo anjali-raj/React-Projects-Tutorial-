c2c266d6cdd77c1bf724ec0e5d121653b3d911c1
Anjali <anjali@Anjali.local>
2021-10-01 21:44:56 +0530

    Authorization Commit

---
+++ Project_Sixteen(Authentication)/src/components/Auth/AuthForm.js
@@ -0,0 +1,101 @@
+import { useState,  useRef, useContext } from 'react';
+import { useHistory } from 'react-router';
+import AuthContext from '../../store/auth-context';
+
+import classes from './AuthForm.module.css';
+
+const AuthForm = () => {
+  const history = useHistory();
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false); 
+
+  const authCtx = useContext(AuthContext);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const emailEntered = emailInputRef.current.value;
+    const passwordEntered = passwordInputRef.current.value;
+
+    //Adding Validation
+    setIsLoading(true);
+    let url;
+    if(isLogin) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBTAoqV7KFAVm-98iX-UeLnCZEdv8KuOI8'
+    }
+    else {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBTAoqV7KFAVm-98iX-UeLnCZEdv8KuOI8'
+    }
+    
+    fetch(url, 
+    {
+      method : 'POST',
+      body: JSON.stringify({
+        email : emailEntered,
+        password : passwordEntered,
+        returnSecureToken : true
+      }),
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    }).then(res => {
+      setIsLoading(false);
+      if (res.ok) {
+        return res.json();
+      }
+      else {
+        return res.json().then(data => {
+          let errorMessage ='Authentication Failed!';
+          // if(data && data.error && data.error.message){
+          //   errorMessage = data.error.message;
+          // } 
+          
+          throw new Error(errorMessage)
+        });
+      }
+    }).then((data) => {
+      const expirationTime = new Date(new Date().getTime() + (+data.expiresIn * 1000));
+      authCtx.login(data.idToken, expirationTime.toISOString());
+      history.replace('/');
+      console.log(data)
+    }).catch(err => {
+      alert(err.message);
+    }
+      );
+  }
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor='email'>Your Email</label>
+          <input type='email' id='email' ref={emailInputRef} required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='password'>Your Password</label>
+          <input type='password' id='password' ref={passwordInputRef} required />
+        </div>
+        <div className={classes.actions}> 
+        {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+        {isLoading && <p> Loading... </p>}
+          <button
+            type='button'
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? 'Create new account' : 'Login with existing account'}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
---
+++ Project_Sixteen(Authentication)/src/components/Profile/ProfileForm.js
@@ -0,0 +1,46 @@
+import { useRef, useContext } from 'react';
+import AuthContext from '../../store/auth-context';
+import classes from './ProfileForm.module.css';
+import { useHistory } from 'react-router';
+
+const ProfileForm = () => {
+  const history = useHistory();
+  const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = event => {
+    event.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBTAoqV7KFAVm-98iX-UeLnCZEdv8KuOI8', {
+       method : 'POST',
+       body: JSON.stringify({
+         idToken : authCtx.token,
+         password : enteredNewPassword,
+         returnSecureToken: false
+       }),
+       headers : {
+         'Content-Type' : 'application/json',
+       },
+    }).then(res => {
+      //let's assume it always succeeds!
+      history.replace('/');
+
+
+    });
+  };
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <div className={classes.control}>
+        <label htmlFor='new-password'>New Password</label>
+        <input type='password' id='new-password' minLength='7' ref={newPasswordInputRef}/>
+      </div>
+      <div className={classes.action}>
+        <button>Change Password</button>
+      </div>
+    </form>
+  );
+}
+
+export default ProfileForm;
